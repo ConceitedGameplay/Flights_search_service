214c82b205204be018555951775d9917358549e3
biswash <just11hiro@gmail.com>
2022-12-12 21:15:18 +0530

    middleware body parser added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express =  require("express");
 const {PORT} = require("./config/severConfig")
+const bodyParser = require("body-parser")
 
 
 const startServer=async()=>{
     
     // create the express object
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     // const PORT =3000;
     app.listen(PORT,()=>{
         console.log(`server started at ${PORT}`)
