53c19e46bfcd8dacb5c702d99979b1305c646068
biswash <just11hiro@gmail.com>
2022-12-17 10:13:22 +0530

    added city controller

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -28,26 +28,67 @@ const create =async(req,res)=>{
 }
 const destroy =async(req,res)=>{
     try {
-        
+        const response = await cityService.deleteCity(req.params.id);
+
+        return res.status(200).json({
+            data:response,
+            success:true,
+            message:"successfully deleted a city",
+            err:{}
+        })
     } catch (error) {
         console.log(`error from controller layer: ${error}`)
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'not able to create a city',
+            err:error
+        })
+           
             
     }
 }
 const get =async(req,res)=>{
     try {
+        const city = await cityService.getCity(req.params.id);
+
+        return res.status(200).json({
+            data:response,
+            success:true,
+            message:"successfully fetched a city",
+            err:{}
+        })
         
     } catch (error) {
         console.log(`error from controller layer: ${error}`)
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'not able to get a city',
+            err:error
+        })
             
     }
 }
 const update =async(req,res)=>{
     try {
         
+        const city = await cityService.updateCity(req.params.id,req.body);
+
+        return res.status(200).json({
+            data:city,
+            success:true,
+            message:"successfully updated a city",
+            err:{}
+        })
     } catch (error) {
         console.log(`error from controller layer: ${error}`)
-            
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'not able to update a city',
+            err:error
+        })
     }
 }
 
