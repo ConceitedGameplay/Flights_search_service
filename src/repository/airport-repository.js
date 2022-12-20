c1402725bfc9f32eb770f417cc13ec159fdc3227
biswash <just11hiro@gmail.com>
2022-12-20 08:48:42 +0530

    airport repository layer added

---
+++ src/repository/airport-repository.js
@@ -0,0 +1,100 @@
+const {Op} = require("sequelize");
+const {Airport} = require("../models/index");
+
+class AirportRepository{
+
+//constructor(){
+
+// }
+
+async createAirport({name,cityId}){
+
+    try {
+       
+        const airport = await Airport.create({
+            name,
+            cityId
+        })
+        return airport;
+    } catch (error) {
+        console.log(`error from repo layer: ${error}`)
+        throw error;
+    }
+}
+
+async bulkCreateAirport(airports){
+    try {
+        const airports = await Airport.bulkCreate([...airports])
+        return airports;
+    } catch (error) {
+        console.log(`error from repo layer : ${error}`)
+        throw error;
+    }
+}
+
+async deleteAirport(airportId){
+    try {
+        await Airport.destroy({
+            where:{
+                id:airportId
+            }
+        })
+    } catch (error) {
+        console.log(`error from repo layer: ${error}`)
+        throw error;
+    }
+}
+
+async updateAirport(airportId,data){
+
+   try {
+    const airport = await Airport.findByPk(airportId)
+    airport.name = data.name;
+    airport.cityId= data.cityId;
+    await airport.save();
+    return airport;
+   } catch (error) {
+     console.log(`error from repo layer: ${error}`)
+     throw error;
+   }
+}
+
+async getAirport(airportId){
+    try {
+        const airport = await Airport.findOne({
+            where:{
+                id:airportId
+            }
+        })
+        return airport;
+    } catch (error) {
+        console.log(`error from repo layer: ${error}`)
+        throw error;
+    }
+}
+
+async getAllAirports(filter){      //can be empty also
+   
+    try {
+         if(filter.name){
+            const airports = await Airport.findAll({
+                where:{
+                    name:{
+                        [Op.startsWith]:filter.name
+                    }
+                }
+            })
+             return airports
+         }
+         const airports = await Airport.findAll()
+         return airports;
+
+    } catch (error) {
+        console.log(`error from repo layer: ${error}`)
+        throw error;
+    }
+}
+
+}
+
+module.exports = AirportRepository
