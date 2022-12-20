effef477d01fc721d8df7dcfc2443418afe247d7
biswash <just11hiro@gmail.com>
2022-12-20 08:03:17 +0530

    case added for conditional syncing of db

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express =  require("express");
 const {PORT} = require("./config/severConfig")
 const bodyParser = require("body-parser")
 const ApiRoutes = require('./routes/index');
+const {Airport,City} = require("./models/index")
+const db = require("./models/index")
 
 const startServer=async()=>{
     
@@ -11,9 +13,31 @@ const startServer=async()=>{
     app.use(bodyParser.urlencoded({extended:true}));
     // const PORT =3000;
     app.use('/api',ApiRoutes);
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`server started at ${PORT}`)
        
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true})
+        }
+        // const city = await City.findOne({
+        //     where:{
+        //         id:12
+        //     }
+        // })
+        // // const newAirport = await Airport.create({name:"Jindal vijaynagar airport",cityId:5})
+        // const airports = await city.getAirports();
+
+        // // await city.addAirport({
+        // //     name:" "
+        // // })
+
+        // const newAirport =  await Airport.findOne({
+        //     where:{
+        //         id:5
+        //     }
+        // })
+        // await city.addAirport(newAirport)
+        // console.log(airports)
     })
 } 
 
