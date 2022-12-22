2ce5b5f9973627fcf80c8b829a92dbee02848a29
biswash <just11hiro@gmail.com>
2022-12-22 22:03:18 +0530

    migration of flights done

--- src/models/flights.js
+++ src/models/flights.js
@@ -13,19 +13,47 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Flights.init({
-    flightNumber: DataTypes.STRING,
-    airplaneId: DataTypes.INTEGER,
-    departureAirportId: DataTypes.INTEGER,
-    arrivalAirportId: DataTypes.INTEGER,
-    arrivalTime: DataTypes.DATE,
-    departureTime: DataTypes.DATE,
-    price: DataTypes.INTEGER,
-    boardingGate: DataTypes.STRING,
-    totalSeats: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Flights',
-  });
+  Flights.init(
+    {
+      flightNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      airplaneId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      departureAirportId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      arrivalAirportId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      arrivalTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      departureTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      boardingGate: DataTypes.STRING,
+      totalSeats: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Flights",
+    }
+  );
   return Flights;
 };
